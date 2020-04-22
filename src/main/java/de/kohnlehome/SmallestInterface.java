91e7994154dd0bb46bd1e9b57916a651d241deec
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-22 12:36:26 +0200

    implemented three testcases

---
+++ src/main/java/de/kohnlehome/SmallestInterface.java
@@ -0,0 +1,8 @@
+package de.kohnlehome;
+
+public class SmallestInterface implements ISmallestInterface{
+
+    public int smallestDifference(int[] array1, int[] array2) {
+        return 0;
+    }
+}
--- src/test/java/de/kohnlehome/AppTest.java
+++ src/test/java/de/kohnlehome/AppTest.java
@@ -4,33 +4,59 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+
 
 
 public class AppTest {
-    private MyInterface myInterface;
+    private ISmallestInterface smallestInterface;
 
     @BeforeEach
     public void init(){
         // ===== ARRANGE =====
         // Testdouble erstellen
-        myInterface = mock(MyInterface.class);
+        smallestInterface = new SmallestInterface();
 
-        // Verhalten des Testdoubles definieren
-        when(myInterface.method(3)).thenReturn(7);
     }
 
     @Test
-    public void test(){
+    public void test1(){
         // ===== ACT =====
         // zu testende Methode aufrufen
-        int x = myInterface.method(3);
+        int[] array1 = {1,3,15,11,2};
+        int[] arary2 = {23,127,235,19,8};
+        int result = smallestInterface.smallestDifference(array1,arary2);
 
         // ===== ASSERT =====
         // Überprüfen, ob Methode richtigen Wert zurückgibt
-        assertThat(x).isEqualTo(7);
+        assertThat(result).isEqualTo(3);
 
-        // Überprüfen, ob Methoden des Testdoubles aufgerufen wurden
-        verify(myInterface).method(3);
+    }
+
+    @Test
+    public void test2(){
+        // ===== ACT =====
+        // zu testende Methode aufrufen
+        int[] array1 = {1,3,15,11,2};
+        int[] arary2 = {23,127,235,12,177};
+        int result = smallestInterface.smallestDifference(array1,arary2);
+
+        // ===== ASSERT =====
+        // Überprüfen, ob Methode richtigen Wert zurückgibt
+        assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    public void test3(){
+        // ===== ACT =====
+        // zu testende Methode aufrufen
+        int[] array1 = {1,6,8,22};
+        int[] arary2 = {188,240,237,50};
+        int result = smallestInterface.smallestDifference(array1,arary2);
+
+        // ===== ASSERT =====
+        // Überprüfen, ob Methode richtigen Wert zurückgibt
+        assertThat(result).isEqualTo(28);
+
     }
 }
