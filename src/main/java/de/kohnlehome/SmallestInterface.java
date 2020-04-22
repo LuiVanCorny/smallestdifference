efd105080d774ae91afc2cef4989ea6ac967d8a5
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-22 13:33:14 +0200

    refactoring

--- src/main/java/de/kohnlehome/SmallestInterface.java
+++ src/main/java/de/kohnlehome/SmallestInterface.java
@@ -5,7 +5,7 @@ public class SmallestInterface implements ISmallestInterface{
     public int smallestDifference(int[] array1, int[] array2) {
         int smallestDiff = Math.abs(array1[0]- array2[0]);
 
-        for(int i = 1; i<array1.length;i++){
+        for(int i = 0; i<array1.length;i++){
             for(int y = 0; y<array2.length;y++){
                 int diff = Math.abs(array1[i]-array2[y]);
                 if(diff<smallestDiff){
--- src/test/java/de/kohnlehome/SmallestInterfaceTest.java
+++ src/test/java/de/kohnlehome/SmallestInterfaceTest.java
@@ -37,8 +37,8 @@ public class SmallestInterfaceTest {
         // ===== ACT =====
         // zu testende Methode aufrufen
         int[] array1 = {1,3,15,11,2};
-        int[] arary2 = {23,127,235,12,177};
-        int result = smallestInterface.smallestDifference(array1,arary2);
+        int[] array2 = {23,127,235,12,177};
+        int result = smallestInterface.smallestDifference(array1,array2);
 
         // ===== ASSERT =====
         // Überprüfen, ob Methode richtigen Wert zurückgibt
@@ -51,8 +51,8 @@ public class SmallestInterfaceTest {
         // ===== ACT =====
         // zu testende Methode aufrufen
         int[] array1 = {1,6,8,22};
-        int[] arary2 = {188,240,237,50};
-        int result = smallestInterface.smallestDifference(array1,arary2);
+        int[] array2 = {188,240,237,50};
+        int result = smallestInterface.smallestDifference(array1,array2);
 
         // ===== ASSERT =====
         // Überprüfen, ob Methode richtigen Wert zurückgibt
